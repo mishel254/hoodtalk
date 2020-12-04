03337a37eef35c6954918886038da97596386439
BihawaM <bihawam271@gmail.com>
2020-12-04 09:17:47 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NeighborhoodComponent } from './neighborhood/neighborhood.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NeighborhoodComponent,
+ 
+  
   ],
   imports: [
     BrowserModule,
