b26414be87d1d606173e96ee09baa4e3fd8cc988
Munge Ryan <mungeryan@gmail.com>
2020-12-06 17:05:53 +0300

    routes to navigate the page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AllHoodsComponent } from './component/all-hoods/all-hoods.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '',pathMatch: 'full'},
+  { path: 'all-hoods', component: AllHoodsComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
