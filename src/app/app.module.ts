38d6eeef3e20bc85b5e1e0ef2486caaf357610a2
BihawaM <bihawam271@gmail.com>
2020-12-06 21:35:55 +0300

    forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 import { NeighborhoodComponent } from './neighborhood/neighborhood.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HttpClientModule } from '@angular/common/http';
-// import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 // import { ShowNeighborhoodComponent } from './neighborhood/show-neighborhood/show-neighborhood.component';
 // import { AddEditNeighborhoodComponent } from './neighborhood/add-edit-neighborhood/add-edit-neighborhood.component';
 
